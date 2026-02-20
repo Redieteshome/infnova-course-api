82d3eb866d60ed587f3e4754fa1b8694fb40cb39
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2026-02-20 22:44:23 +0300

    added unit test

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesService } from './courses.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('CoursesService', () => {
   let service: CoursesService;
@@ -12,7 +13,29 @@ describe('CoursesService', () => {
     service = module.get<CoursesService>(CoursesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findOne', () => {
+    it('should return a course if found', () => {
+      const course = service.findOne(1);
+      expect(course).toBeDefined();
+      expect(course.id).toBe(1);
+    });
+
+    it('should throw NotFoundException if course does not exist', () => {
+      expect(() => service.findOne(999)).toThrow(NotFoundException);
+    });
+  });
+
+  describe('create', () => {
+    it('should create and return a new course', () => {
+      const newCourse = service.create({
+        title: 'NestJS Basics',
+        level: 'Intermediate',
+        duration: '6 weeks',
+      });
+
+      expect(newCourse).toBeDefined();
+      expect(newCourse.id).toBeGreaterThan(0);
+      expect(newCourse.title).toBe('NestJS Basics');
+    });
   });
 });
